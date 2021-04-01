df761cbc4de8736778dfab5393b7ae71cf026b9d
TeamCode708 <78527536+TeamCode708@users.noreply.github.com>
2021-04-01 19:39:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,14 +11,22 @@ recognition.onresult=function run(event) {
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
+    if (Content == "take my selfie") {
+        console.log("Taking selfie---");
+        speak();
+    }
 }
 
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking Your Selfie In Five Seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function() {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 Webcam.set({
@@ -29,3 +37,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri+'">';
+    });
+}
+
+function save() { 
+    link = document.getElementById("link"); 
+    image = document.getElementById("selfie_img").src;
+    link.href = image; 
+    link.click(); 
+}
